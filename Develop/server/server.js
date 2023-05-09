a9b8820e550632e3a756a13abd742507df23cd52
dleatonsalinas <daniellesalinas721@gmail.com>
2023-05-09 10:55:37 -0500

    apply Apollo Server as middleware to Express in server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -4,14 +4,19 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
+
+// Apply Apollo Server as middleware to Express
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
